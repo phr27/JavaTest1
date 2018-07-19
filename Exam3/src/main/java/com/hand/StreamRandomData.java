dfe7058783b7d5ef563b2ae7c1779da9233a6462
phr27 <phr27@163.com>
2018-07-19 11:56:19 +0800

    冒泡排序解决一个bug

--- Exam3/src/main/java/com/hand/StreamRandomData.java
+++ Exam3/src/main/java/com/hand/StreamRandomData.java
@@ -5,7 +5,7 @@ public class StreamRandomData extends AbsRandomData {
     @Override
     public void sort() {
         data.values().stream().forEach(nums -> {
-            for (int i = 1; i < nums.size() - 1; i++) {
+            for (int i = 1; i < nums.size(); i++) {
                 for (int j = 0; j < nums.size() - i; j++) {
                     if (nums.get(j) > nums.get(j + 1)) {
                         int swap = nums.get(j);
